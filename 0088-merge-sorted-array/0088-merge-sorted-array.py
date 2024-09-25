cd4b570e0bf33df247dda31ad53822cb2725238b
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2024-09-25 13:15:42 -0400

    Time: 36 ms (77.95%), Space: 16.7 MB (10.32%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,29 +1,28 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        if(m==0):
-            p1=0
-            for a in nums2:
-                nums1.insert(p1,a)
-                nums1.pop()
-                p1+=1
+        
+        p1=m-1
+        p2=n-1
+        pp=m+n-1
 
-        elif(n==0):
-            pass
-        else:
-            print('hi')
-            p2=p1=0
-            while p1<len(nums1) and  p2<n:
-                if( nums2[p2]<=nums1[p1]):
-                    nums1.insert(p1,nums2[p2])
-                    nums1.pop()
-                    p2+=1
+        while p1>=0 and p2>=0:
+            if(nums1[p1]>nums2[p2]):
+                nums1[pp] = nums1[p1]
+                p1-=1
+            else:
+                nums1[pp]= nums2[p2]
+                p2-=1
+            pp-=1
+        
+        print(nums1,p2,p1,pp)
 
-                p1+=1
-            p1=m+p2  
-            while p2<n:
-                nums1.insert(p1,nums2[p2])
-                nums1.pop()
-                p1+=1
-                p2+=1
+        while p2>=0:
+            nums1[pp]=nums2[p2]
+            p2-=1
+            pp-=1
+
+
+
+
 
 
