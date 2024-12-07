a3711eb3e2bf1fb75340ec84295273b7fa742447
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2024-12-07 17:49:45 -0500

    Time: 1684 ms (5.03%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,16 @@
+/* Write your T-SQL query statement below */
+
+
+
+    
+
+select a.name 
+from employee a
+inner join (
+    select aa.managerid, count(*) cnt
+    from employee aa
+    where aa.managerid is not null
+    group by aa.managerid
+) as b
+on a.id = b.managerid
+where b.cnt >=5
