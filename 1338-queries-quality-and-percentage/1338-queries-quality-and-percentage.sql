8f88f0028edd890186cc73827183be9b92d16a92
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2024-12-09 15:19:31 -0500

    Time: 1192 ms (38.1%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,27 +1,11 @@
 /* Write your T-SQL query statement below */
 
-WITH cte AS (
-    SELECT 
-        q1.query_name, 
-        ROUND((ISNULL(a.less, 0) * 1.0 / COUNT(q1.rating)) * 100, 2) AS poor_query_percentage
-    FROM queries q1
-    LEFT JOIN (
-        SELECT 
-            q2.query_name, 
-            COUNT(rating) AS less
-        FROM queries q2
-        WHERE rating < 3
-        GROUP BY q2.query_name
-    ) AS a
-    ON a.query_name = q1.query_name
-    GROUP BY q1.query_name, a.less
-),
-cte2 as (
-    select query_name , round(avg(rating*1.0/position),2) quality
-    from queries
-    group by query_name
-)
-select cte.query_name, cte2.quality, cte.poor_query_percentage 
-from cte2
-inner join cte
-on cte.query_name = cte2.query_name
+
+select query_name,
+
+round(avg(rating*1.0/position),2) quality ,
+
+round(100*1.0*count(case when rating < 3 then 1 end)/count(rating),2) poor_query_percentage 
+
+from queries
+group by query_name
