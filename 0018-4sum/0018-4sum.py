7133eb95cb71ea1a5c96c5ca1f9ca3fb4bf9190d
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2024-12-09 19:47:59 -0500

    Time: 319 ms (76.4%), Space: 17.4 MB (10.78%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,32 +2,34 @@ from typing import List
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
         res = []
+        quad = []
+        nums.sort()
 
-        for i in range(len(nums) - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in range(i + 1, len(nums) - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                p1, p2 = j + 1, len(nums) - 1
-                while p1 < p2:
-                    curr_sum = nums[i] + nums[j] + nums[p1] + nums[p2]
-                    if curr_sum == target:
-                        res.append([nums[i], nums[j], nums[p1], nums[p2]])
+        def recur(k,start,target):
+            if k!=2:
+                for i in range(start,len(nums)-k+1):
+                    if i>start and nums[i] == nums[i-1]:
+                        continue
+                    quad.append(nums[i])
+                    recur(k-1,i+1,target-nums[i])
+                    quad.pop()
+                return
+        
+            p1,p2 = start,len(nums)-1  
+            while p1<p2:
+                if nums[p1]+nums[p2]<target:
+                    p1+=1
+                elif(nums[p1]+nums[p2]>target):
+                    p2-=1
+                else:
+                    res.append(quad + [nums[p1],nums[p2]])
+                    p1+=1
+                    
+                    while p1 < p2 and nums[p1] == nums[p1 - 1]:
                         p1 += 1
-                        p2 -= 1
-                        # Skip over duplicates for p1
-                        while p1 < p2 and nums[p1] == nums[p1 - 1]:
-                            p1 += 1
-                        # Skip over duplicates for p2
-                        while p1 < p2 and nums[p2] == nums[p2 + 1]:
-                            p2 -= 1
-                    elif curr_sum < target:
-                        p1 += 1
-                    else:
-                        p2 -= 1
+                    
+        recur(4,0,target)
 
         return res
 
