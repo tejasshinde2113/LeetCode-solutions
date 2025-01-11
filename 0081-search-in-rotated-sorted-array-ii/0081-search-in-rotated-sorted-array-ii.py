a53dc341439146ca30b1af28647bffcf17c91475
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-11 10:17:12 -0500

    Time: 0 ms (100%), Space: 18.3 MB (23%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -2,27 +2,29 @@ class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         low, high = 0, len(nums) - 1
 
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] == target:
+        start=0
+        end = len(nums)-1
+        while start<=end:
+            mid = start + (end-start)//2
+            if (nums[mid]==target):
                 return True
-         
-            if nums[low] == nums[mid]:
-                low += 1
+            if(nums[start]== nums[mid]):
+                start = start+1
                 continue
-            if nums[high] == nums[mid]:
-                high -= 1
+            elif(nums[mid] == nums[end]):
+                end = end -1
                 continue
-            
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    high = mid - 1
+
+            if(nums[start]<= nums[mid]):
+                if(nums[start]<=target<nums[mid]):
+                    end = mid - 1
                 else:
-                    low = mid + 1
+                    start = mid+1
             else:
-                if nums[mid] <= target <= nums[high]:
-                    low = mid + 1
+                if(nums[mid]<target<=nums[end]):
+                    start = mid+1
                 else:
-                    high = mid - 1
-        
+                    end = mid-1
+
+
         return False
