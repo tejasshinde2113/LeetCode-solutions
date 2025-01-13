6e0b5783c1e8380d2223f6ff27b2c48bb9011c90
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-13 16:11:10 -0500

    Time: 402 ms (9.83%), Space: 18.7 MB (63.93%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -1,9 +1,7 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
         count = {}
-        cnt=0
         for a in s:
-
             count[a] = 1+ count.get(a,0)
             if count[a]==3:
                 count[a]-=2
