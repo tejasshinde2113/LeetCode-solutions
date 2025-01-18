7476eb215607d1a35c5c156235f04ab9d3fbe6ff
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-18 14:46:29 -0500

    Time: 163 ms (9.66%), Space: 32.1 MB (37.61%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -10,38 +10,38 @@ class Solution:
         if not root:
             return 0
 
-        res=[0]
+        # res=[0]
 
-        def dfs(root,comp):
-            if not root:
-                return
+        # def dfs(root,comp):
+        #     if not root:
+        #         return
             
-            if root.val >= comp:
-                res[0] = res[0]+1
+        #     if root.val >= comp:
+        #         res[0] = res[0]+1
             
-            dfs(root.left,max(root.val,comp))
-            dfs(root.right,max(root.val,comp))
+        #     dfs(root.left,max(root.val,comp))
+        #     dfs(root.right,max(root.val,comp))
 
-        dfs(root,root.val-1)
-        return res[0]
+        # dfs(root,root.val-1)
+        # return res[0]
 
-        # res =0
-        # if not root:
-        #     return res
-        # q=deque([(root,root.val -1)])
+        res =0
+        if not root:
+            return res
+        q=deque([(root,root.val -1)])
 
-        # while q:
+        while q:
 
-        #     temp,comp= q.popleft()
+            temp,comp= q.popleft()
 
-        #     if temp.val >= comp:
-        #         res+=1
+            if temp.val >= comp:
+                res+=1
             
-        #     if temp.left:
-        #         q.append((temp.left, max(temp.val,comp)))
-        #     if temp.right:
-        #         q.append((temp.right, max(temp.val,comp)))
-        # return res
+            if temp.left:
+                q.append((temp.left, max(temp.val,comp)))
+            if temp.right:
+                q.append((temp.right, max(temp.val,comp)))
+        return res
 
 
 
