82f6879286732d1527442b4e5909c580034d74e8
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-24 12:42:50 -0500

    Time: 3022 ms (25.55%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 
-select isnull(u.unique_id,null) unique_id , e.name
+select u.unique_id unique_id , e.name
 from employees e 
 left join employeeUNI u
 on u.id = e.id
