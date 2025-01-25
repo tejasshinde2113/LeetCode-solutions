24e644226488b2f07ff3869f2f4a52ade8b57940
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-25 10:39:49 -0500

    Time: 3 ms (89.28%), Space: 19.6 MB (16.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
         adj = [[] for _ in range(numCourses)]
         for course, prereq in prerequisites:
             adj[prereq].append(course)
         visit = [0 for a in range(numCourses)]
         path = [0 for a in range(numCourses)]
+        
 
-
-        def dfs(node,adj, visit,path):
-            
-            visit[node] = 1
-            path[node] = 1
-          
-            for a in adj[node]:
+        def dfs(root):
+            visit[root] = 1
+            path[root]=1
+            for a in adj[root]:
                 if not visit[a]:
-                    if dfs(a,adj,visit,path) == True:
+                    if dfs(a):
                         return True
-                elif(path[a]==1):
+                elif path[a]==1:
                     return True
-            path[node]=0
+            path[root]=0
             return False
+                    
+
+
 
         for a in range(numCourses):
             if not visit[a]:
-                if  dfs(a,adj,visit,path):
+                visit[a] = 1
+                path[a]=1
+                if dfs(a):
                     return False
         return True
-
-
-
-        
