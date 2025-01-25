74275cb8e6ade1109546a2cc34ea9ea2541021e4
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-25 12:14:13 -0500

    Time: 301 ms (28.95%), Space: 38.7 MB (67.67%) - LeetHub

---
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
@@ -0,0 +1,37 @@
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+
+
+        res = -1
+        visit = [0 for a in range(len(edges))]
+        
+        def bfs(i):
+            path = {}
+            q = deque([[i,1]])
+            while q:
+                t,num= q.popleft()
+                
+                if edges[t] > -1:
+                    
+                    if edges[t] in path:
+                        return num - path[edges[t]] +1
+                    if visit[edges[t]] ==1:
+                        return -1
+                   
+                    q.append([edges[t], num + 1])
+                    path[t] = num
+                    visit[t] = 1
+                else:
+                    return -1
+            return -1
+
+
+        for i,a in enumerate(visit):
+            if a==0:  
+                temp = bfs(i)
+                res = max(temp,res)
+                
+            
+        return res 
+
+        
