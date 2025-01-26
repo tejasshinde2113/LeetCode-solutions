18ac9b790af22676b60542be365958ef9874c205
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-26 13:20:13 -0500

    Time: 31 ms (19.39%), Space: 31.8 MB (8.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,46 +1,39 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        m = len(board)
         n = len(board[0])
+        m = len(board)
+        
+        visit=[[0 for a in range(len(board[0]))] for b in range(len(board)) ]
+        st=[]
+        border=[False]
 
-        visit = [[0 for a in range(n)] for b in range(m)]
-        temp = visit
-        def bfs(r,c):
+        def dfs(row,col):
+            visit[row][col]=1
+
+
+            for x,y in [(1,0),(0,1),(0,-1),(-1,0)]:
+                trow = row+x
+                tcol = col+y
+                
+
+                if 0<=trow< m and 0<=tcol<n and not visit[trow][tcol] and board[trow][tcol]=='O':
+                    dfs(trow,tcol)
+            
+            if row == 0 or col == 0 or row == m-1 or col == n-1:
+                border[0]=True
+            st.append((row,col))
             
 
-            q=[(r,c)]
-            while q:
-                row,col = q.pop()
-                visit[row][col]= 1
-                temp[row][col]=1
-
-                for x,y in [(-1,0),(1,0),(0,-1),(0,1)]:
-                    temprow = row+x
-                    tempcol = col+y
-
-                    if 0<=temprow<m and 0<=tempcol<n and  not visit[temprow][tempcol] and board[temprow][tempcol] == "O":
-                        q.append((temprow,tempcol))
-
-
-        for r in range(m):
-            for c in range(n):
-                if not visit[r][c] and board[r][c] == "O":
-                    bfs(r,c)
-                    f= True
-                    for a in range(m):
-                        for b in range(n):
-                            if not (0 < a < m-1) and temp[a][b]==1:
-                                f= False
-                            if not (0 < b < n-1) and temp[a][b]==1:
-                                f= False
-                    if f:
-                        for a in range(m):
-                            for b in range(n):
-                                if temp[a][b]==1:
-                                    board[a][b]='X'
-
-
-                    temp = [[0 for a in range(n)] for b in range(m)]
-
-
+        for i,row in enumerate(board):
+            for j,col in enumerate(row):
+                if col == 'O' and visit[i][j] == 0:
+                    dfs(i,j)
+                    
+                    if not border[0]:
+                        for row,col in st:
+                            board[row][col] = 'X'
+                    border[0] = False
+                    st=[]
+        
+        
         
