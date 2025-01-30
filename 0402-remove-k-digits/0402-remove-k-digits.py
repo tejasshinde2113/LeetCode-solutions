eee396d64ed45b99ecac3415a79e756d1c8b511c
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-01-29 19:05:51 -0500

    Time: 19 ms (76.69%), Space: 19.1 MB (23.03%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,5 +1,7 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
+        if k >= len(num):
+            return '0'
         st=[]
         temp = k
         for a in num:
@@ -8,7 +10,9 @@ class Solution:
                 st.pop()
                 k-=1
             st.append(a)
-        st = st[:len(num)-temp]
+        if k>0:
+            st=st[:len(num)-temp]
+       
         
         return ''.join(st).lstrip('0') or '0'
 
