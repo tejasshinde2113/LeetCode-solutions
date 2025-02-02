b4348deabeacc52a6a4172d327b37fa4e1f68ae4
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-02 10:34:00 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,36 +1,36 @@
-from typing import List
-
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res = []
-        quad = []
         nums.sort()
-
+        res=[]
+        fixed =[]
         def recur(k,start,target):
-            if k!=2:
+
+            if k != 2:
                 for i in range(start,len(nums)-k+1):
                     if i>start and nums[i] == nums[i-1]:
                         continue
-                    quad.append(nums[i])
+                    fixed.append(nums[i])
                     recur(k-1,i+1,target-nums[i])
-                    quad.pop()
+                    fixed.pop()
                 return
-        
-            p1,p2 = start,len(nums)-1  
+            
+            p1=start
+            p2 = len(nums)-1
             while p1<p2:
-                if nums[p1]+nums[p2]<target:
+
+                currSum = sum([fixed[0],fixed[1],nums[p1],nums[p2]])
+                print(currSum)
+
+                if currSum == target:
+                    res.append([fixed[0],fixed[1],nums[p1],nums[p2]])
                     p1+=1
-                elif(nums[p1]+nums[p2]>target):
-                    p2-=1
+                    while p1<p2 and nums[p1]==nums[p1-1]:
+                        p1+=1
+                elif currSum < target:
+                    p1+=1
                 else:
-                    res.append(quad + [nums[p1],nums[p2]])
-                    p1+=1
-                    
-                    while p1 < p2 and nums[p1] == nums[p1 - 1]:
-                        p1 += 1
-                    
+                    p2-=1
+
         recur(4,0,target)
-
         return res
-
-       
+        
