1cff8455e3eb3ed9e312e131f5bcc8eeb5b8068b
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-02 12:17:37 -0500

    Time: 3 ms (38.13%), Space: 18.8 MB (36.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -6,10 +6,8 @@ class Solution:
         while p2<len(prices):
             val = prices[p2] - prices[p1]
             if(val > 0 ):
-                p2+=1
-                p1=p2-1
+                
                 maxval += val
-            elif(val<=0):
-                p2+=1
-                p1 = p2-1
+            p2+=1
+            p1=p2-1
         return maxval
