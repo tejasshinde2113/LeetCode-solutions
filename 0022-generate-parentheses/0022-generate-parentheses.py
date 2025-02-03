7bd01a17559e1cd1a0e9faf4d47c477eb767aa35
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-03 08:49:24 -0500

    Time: 0 ms (100%), Space: 18.2 MB (21.71%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,41 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
 
-
-        res=[]
-        st=[]
-        def parent(opened,close):
-            if opened == close == n:
-                res.append(''.join(st))
-            
-
-            if opened <n:
-                st.append('(')
-                parent(opened+1,close)
-                st.pop()
-            
-            if close <opened:
-                st.append(')')
-                parent(opened,close+1)
-                st.pop()
+        res = []
+        stack = deque([("", 0, 0)]) 
         
-        parent(0,0)
+        while stack:
+            s, opened, closed = stack.pop()
+            
+            if opened == closed == n:
+                res.append(s)
+                continue
+            
+            if opened < n:
+                stack.append((s + "(", opened + 1, closed))
+            
+            if closed < opened:
+                stack.append((s + ")", opened, closed + 1))
+        
         return res
+
+
+        # res=[]
+        # st=[]
+        # def parent(opened,close):
+        #     if opened == close == n:
+        #         res.append(''.join(st))
+            
+
+        #     if opened <n:
+        #         st.append('(')
+        #         parent(opened+1,close)
+        #         st.pop()
+            
+        #     if close <opened:
+        #         st.append(')')
+        #         parent(opened,close+1)
+        #         st.pop()
+        
+        # parent(0,0)
+        # return res
