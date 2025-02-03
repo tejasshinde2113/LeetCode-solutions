ba6c3c999b15dd6e5f8b32a7e13e53eaf8de4207
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-03 10:24:34 -0500

    Time: 2 ms (40.3%), Space: 17.7 MB (65.9%) - LeetHub

---
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.py
@@ -0,0 +1,41 @@
+class Solution:
+    def hasMatch(self, s: str, p: str) -> bool:
+        
+        ind = p.index('*')
+
+        halfCorrect = False
+        first = p[:ind]
+        secind = 0
+        for i in range(len(s)-len(first)+1):
+            if first == s[i:i+len(first)]:
+                halfCorrect = True
+                secind = i+len(first)
+                break
+        
+        if not halfCorrect:
+            return False
+        
+        
+
+        
+
+        sec = p[ind+1:]
+
+        if not sec:
+            return True
+        
+
+        if not first:
+            secind = 0
+        
+        if secind >= len(s):
+            return False
+        
+
+        return True if sec in s[secind:] else False
+        
+
+
+
+
+        
