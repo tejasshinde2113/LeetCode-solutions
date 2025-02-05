9a0fcf99dbab2841930c1d048a72816b04b2046f
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-05 12:46:12 -0500

    Time: 7 ms (6.82%), Space: 17.8 MB (39.28%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        c = Counter(s)
+
+        heap = [[-v,k] for k,v in c.items()]
+
+
+        heapq.heapify(heap)
+        prev =None
+        res = []
+        while heap or prev:
+            if prev and not heap:
+                return ''
+
+            val,char = heapq.heappop(heap)
+            res.append(char)
+
+            val +=1
+
+            if prev:
+                print(prev)
+                heapq.heappush(heap,prev)
+                prev = None
+
+            if val!=0:
+                prev = [val,char]
+        
+        return ''.join(res)
+
+
+
+
+
+        
+        
