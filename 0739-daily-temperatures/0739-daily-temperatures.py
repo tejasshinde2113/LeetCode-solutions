ef5042028722d2d5555f64d3e85bc00da3f18ce5
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-05 16:26:42 -0500

    Time: 151 ms (12.83%), Space: 27.9 MB (45.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -7,23 +7,15 @@ class Solution:
         st=[]
         for i in range(len(temp)-1,-1,-1):
 
+            while st and temp[i] >= st[-1][0]:
+                    st.pop()
             if not st:
                 final.append(0)
                 st.append((temp[i],i))
                 continue
-            if temp[i] < st[-1][0]:
+            else:
                 final.append(st[-1][1]-i)
                 st.append((temp[i],i))
-            else:
-                while st and temp[i] >= st[-1][0]:
-                    st.pop()
-                if not st:
-                    final.append(0)
-                    st.append((temp[i],i))
-                    continue
-                else:
-                    final.append(st[-1][1]-i)
-                    st.append((temp[i],i))
         
         return(final[::-1])
         
