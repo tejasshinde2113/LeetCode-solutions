8cd6aafe9ce5bc35d3dad635fb464db3a79161b7
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-08 10:23:21 -0500

    Time: 379 ms (45.48%), Space: 17.8 MB (77.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,32 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # Transform the string to handle both odd and even length palindromes uniformly
-        T = '#'.join('^{}$'.format(s))  
-        n = len(T)
-        P = [0] * n
-        C = R = 0  # Center and right boundary
-        max_len = 0
-        center_idx = 0
 
-        for i in range(1, n - 1):
-            mirror = 2 * C - i  # Mirror index of i around center C
-            
-            if i < R:
-                P[i] = min(R - i, P[mirror])  # Use previously computed values
-            
-            # Expand around center i
-            while T[i + P[i] + 1] == T[i - P[i] - 1]:
-                P[i] += 1
+        ans =  ''
 
-            # Update center and right boundary
-            if i + P[i] > R:
-                C, R = i, i + P[i]
-            
-            # Track the longest palindrome found
-            if P[i] > max_len:
-                max_len = P[i]
-                center_idx = i
 
-        # Extract longest palindromic substring from the original string
-        start = (center_idx - max_len) // 2  # Convert back to original indices
-        return s[start: start + max_len]
+        def check(l,r):
+            nonlocal ans
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                if r-l+1 > len(ans):
+                    ans = s[l:r+1]
+                l-=1
+                r+=1
+        for i in range(len(s)):
+
+            check(i,i)
+            check(i,i+1)
+        return ans
+            
+            
