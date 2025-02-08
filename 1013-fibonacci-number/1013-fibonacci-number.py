8f4a73fb15f50f38fdddd4458216b580727143c1
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-08 11:16:16 -0500

    Time: 35 ms (80.04%), Space: 17.9 MB (21.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,16 +1,14 @@
 class Solution:
+    def __init__(self):
+
+        self.hm = {}
     def fib(self, n: int) -> int:
-        if n==0:
+        if n in self.hm:
+            return self.hm[n]
+        if n ==0:
             return 0
         if n==1:
             return 1
-        if n==2:
-            return 1
         
-        lst = [1,1]
-        res = 0
-        for i in range(2,n):
-            res = lst[i-1]+lst[i-2]
-            lst.append(res)
-        
-        return res
+        self.hm[n]=self.fib(n-1)+self.fib(n-2)
+        return self.hm[n]
