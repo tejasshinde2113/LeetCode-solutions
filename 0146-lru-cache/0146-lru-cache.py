2272045779684b4258842560fe36f867b06aaa17
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-08 09:44:26 -0500

    Time: 169 ms (26.64%), Space: 78.7 MB (13.47%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,63 +1,55 @@
 class Node:
-    def __init__(self,key,val):
+    def __init__(self, key,val):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
-
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cap = capacity
         self.dct ={}
-        self.capacity = capacity
-        
         self.left = Node(0,0)
         self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
-
-    def insert(self,element):
-        back = self.right.prev 
-        front = back.next
-
-        back.next = element
-        element.prev = back
-        element.next = front
-        front.prev = element
+        
+    def insert(self,comp):
+        p = self.right.prev 
+        p.next = comp
+        comp.prev = p
+        comp.next = self.right
+        self.right.prev = comp
     
-    def remove(self,element):
+    def remove(self,comp):
+        n = comp.prev
 
-        prev_node = element.prev
-        next_node = element.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
+        n.next = comp.next
+        comp.next.prev = n
 
     def get(self, key: int) -> int:
         if key in self.dct:
             self.remove(self.dct[key])
             self.insert(self.dct[key])
             return self.dct[key].val
-        return -1
-        
+        return -1    
 
     def put(self, key: int, value: int) -> None:
+        
         if key in self.dct:
             self.remove(self.dct[key])
-            del self.dct[key] 
-
+            del self.dct[key]
+        
         self.dct[key] = Node(key,value)
-        self.insert(self.dct[key]) 
+        self.insert(self.dct[key])
 
-        if len(self.dct) > (self.capacity):
-            
-            least = self.left.next
-            self.remove(least)
-
-            del self.dct[least.key]
-
-
+        if len(self.dct) > self.cap:
+            l = self.left.next
+            self.remove(l)
+            del self.dct[l.key]
+        
+        
         
 
 
