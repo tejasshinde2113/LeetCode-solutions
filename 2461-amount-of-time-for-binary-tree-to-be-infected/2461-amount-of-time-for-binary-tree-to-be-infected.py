e803923c0cb0ab007d1c0c5a402516c34ba3041d
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-08 21:51:35 -0500

    Time: 133 ms (83.87%), Space: 54.8 MB (80.53%) - LeetHub

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        if not root.left and not root.right:
+            return 0
+        temp = root
+
+        q=deque([(root,None)])
+        par ={}
+        res = None
+        while q:
+            node,parent = q.popleft()
+            if start == node.val:
+                res = node
+
+
+            par[node] = parent
+
+            if node.left:
+                q.append((node.left, node))
+
+            if node.right:
+                q.append((node.right,node))
+
+        q=deque([res])
+        s =set()
+        cnt=0
+        while q:
+            cnt+=1
+            for a in range(len(q)):
+                node = q.popleft()
+                s.add(node)
+
+                if node.left and not node.left in s:
+                    q.append(node.left)
+                
+                if par[node] and not par[node] in s:
+                    q.append(par[node])
+
+                if node.right and not node.right in s:
+                    q.append(node.right)
+            
+            
+        return cnt-1
+        
+        
+
+        
