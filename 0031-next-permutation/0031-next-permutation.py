869cf9bfa685fef1530153ac0b56fbd5af265219
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-11 12:34:09 -0500

    Time: 0 ms (100%), Space: 17.8 MB (52.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,59 +1,34 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-
-        def reverseString(nums):
-            for a in range(len(nums)//2):
-                temp = nums[a]
-                nums[a] = nums[len(nums)-1-a]
-                nums[len(nums)-1-a] = temp
-
-        def partial_in_place_sort(arr, start_index):
-            for i in range(start_index + 1, len(arr)):
-                key = arr[i]
-                j = i - 1
-                while j >= start_index and arr[j] > key:
-                    arr[j + 1] = arr[j]
-                    j -= 1
-                arr[j + 1] = key
         
-        f=-1
-        for i in range(len(nums)-1,-1,-1):
-            if(i>0):
-                if(nums[i]>nums[i-1]):
-                    f=i-1
-                    break
-        print(f)
-        f2=0
-        val = len(nums)-1
-        if f==-1:
-            reverseString(nums)
-            nums = nums[::-1]
-        else:
-            for i,a in enumerate(nums):
-                if i<f+1:
-                    continue
-                if(a>nums[f]):
+        peak = -1
 
-                    if(f2==0):
-                        val = i
-                        f2=1
-                    if(f2==1 and nums[i]<nums[val]):
-                        val = i
-     
-            temp = nums[f]
-    
-            nums[f] = nums[val]
- 
-            nums[val] = temp
-      
-            partial_in_place_sort(nums, f+1)
-     
+        n = len(nums)-1
+        while n>0:
+            if nums[n] > nums[n-1]:
+                peak = n
+                break
+            n-=1
+        if peak == -1:
+            l = 0
+            r = len(nums) -1
 
-
+            while l<r:
+                nums[l],nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+            return 
 
         
+        ind = peak
 
+        for i in range(peak,len(nums)):
+            if nums[i]> nums[peak - 1] and nums[i]<nums[ind]:
+                ind = i
         
+        nums[peak-1],nums[ind] = nums[ind],nums[peak-1]
 
-        
-        
+        nums[peak:] = sorted(nums[peak:])
+        return
+
+
