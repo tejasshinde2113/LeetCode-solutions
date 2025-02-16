4f71c1ab8404bdbaa87b236d71fad69a6038d0e9
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-16 12:39:34 -0500

    Time: 3 ms (83.03%), Space: 17.8 MB (72.1%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,51 +1,42 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
         m = len(grid)
         n = len(grid[0])
-        q=deque()
-        oneCnt =0
+        ones =0
 
+        q=deque([])
 
-        visit = [[0 for a in range(n)] for b in range(m) ]
+        visit = [[0 for i in range(n)] for j in range(m)]
 
-        
-        def bfs(q):
-            nonlocal oneCnt
-            cnt =0
-        
-
-            while q and oneCnt>0:
-                cnt+=1
-                for a in range(len(q)):
-                    row,col = q.popleft()
-
-                    visit[row][col]=1
-
-                    for x,y in [(0,1),(1,0),(-1,0),(0,-1)]:
-                        tempr = row+x
-                        tempc = col+y
-                        if   0<=tempr<m and 0<=tempc<n and not visit[tempr][tempc] and grid[tempr][tempc] ==1:
-                            visit[tempr][tempc]=1
-                            grid[tempr][tempc] =2
-                            oneCnt -=1
-                            q.append((tempr,tempc))
-                
-            return cnt
-
-
-
-        for i, row in enumerate(grid):
+        for i,row in enumerate(grid):
             for j,col in enumerate(row):
-                if col == 2 :
+
+                if col ==2:
+                    visit[i][j]=1
                     q.append((i,j))
                 elif col ==1:
-                    oneCnt+=1
+                    ones+=1
         
-        
-        maxall=bfs(q)
-        
-        if oneCnt>0:
-            return -1
-        return maxall if maxall >-1 else 0 
-        
+
+        cnt=0
+        while q and ones:
+            for _ in range(len(q)):
+                row,col = q.popleft()
+
+                visit[row][col]=1
+
+
+                for x,y in [(1,0),(0,1),(-1,0),(0,-1)]:
+
+
+                    tr = row+x
+                    tc = col+y
+                
+                    if 0<=tr<m and 0<=tc<n and visit[tr][tc]==0 and grid[tr][tc]==1:
+                        ones-=1
+                        grid[tr][tc]=2
+                        visit[tr][tc]=1
+                        q.append((tr,tc))
+            cnt+=1
+
+        return cnt if not ones else -1
