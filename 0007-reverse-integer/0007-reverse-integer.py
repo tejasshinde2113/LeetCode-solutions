498e7c76ed7561de44e4b1c88b0741eb54ea28e2
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-16 15:06:28 -0500

    Time: 45 ms (18.91%), Space: 17.9 MB (16.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -2,20 +2,24 @@ class Solution:
     def reverse(self, x: int) -> int:
         
         lmax = 2147483647
-        lmin = -2147483648
+        lmin = 2147483648
 
         res =0
 
-        while x:
-            temp = int(math.fmod(x,10))
-            x = int(x/10)
+        neg = False
+        if x <0:
+            neg = True
+            x = -x
 
-            if res > int(lmax/10) or (res == int(lmax/10) and temp > int(lmax%10)):
-                return 0
-        
-            if res < int(lmin/10) or (res == int(lmin/10) and temp < int(math.fmod(lmin,10))):
-                return 0
-            
+        temp =x
+        ans = 0
+        while temp >0:
+            digit = temp%10
+            ans = ans*10 + digit
 
-            res = res*10+temp
-        return res
+            if not neg and  ans/10 > lmax/10 or (ans == lmax/10 and  ans%10 > lmax%10):
+                return 0
+            if neg and  ans/10 > lmin/10 or (ans == lmin/10 and  ans%10 > lmin%10):
+                return 0
+            temp = temp//10
+        return ans if not neg else -ans
