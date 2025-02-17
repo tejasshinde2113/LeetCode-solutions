063a4910c86c971e4736f3b9d56da0ca383b65d1
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-17 11:26:33 -0500

    Time: 9 ms (67.85%), Space: 17.8 MB (61.31%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,23 @@
 class Solution:
     def combinationSum(self, ca: List[int], target: int) -> List[List[int]]:
 
+
         res =[]
-        def check(s,arr,ind):
-            if s> target:
-                return False
-            if s== target:
-                res.append(arr)
+
+
+        def check(total,ind,arr):
+
+            if total == target:
+                res.append(arr[:])
                 return
+            elif total > target:
+                return 
+
             
             for i in range(ind,len(ca)):
-                check(ca[i]+s,arr + [ca[i]]   ,i)
-        check(0,[],0)
+                check(total+ca[i],i, arr+ [ca[i]])
+        
+
+        check(0,0,[])
         return res
         
