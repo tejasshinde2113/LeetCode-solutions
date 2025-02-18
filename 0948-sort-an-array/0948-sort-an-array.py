62550ef74966503b9e6d0783addbb6459b828fea
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-18 11:59:46 -0500

    Time: 110 ms (83.15%), Space: 24.7 MB (60.1%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,33 +1,10 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
 
-        def combine(l,r,mid):
+        heapq.heapify(nums)
 
-            temp1 = nums[l:mid+1] +[float('inf')]
-            temp2 = nums[mid+1:r+1] +[float('inf')]
-            res =[]
-            p1=0
-            p2=0
-            for i in range(l,r+1):
-                if temp1[p1]>temp2[p2]:
-                    res.append(temp2[p2])
-                    p2+=1
-                else:
-                    res.append(temp1[p1])
-                    p1+=1
-            for i in range(l, r + 1):
-                nums[i] = res[i - l]
-            
-            return nums
-
-
-        def mergesort(l,r):
-            if l<r:
-                m = l+(r-l)//2
-                mergesort(l,m)
-                mergesort(m+1,r)
-
-                return combine(l,r,m)
+        res = []
+        while nums:
+            res.append(heapq.heappop(nums))
         
-        (mergesort(0,len(nums)-1))
-        return nums
+        return res
