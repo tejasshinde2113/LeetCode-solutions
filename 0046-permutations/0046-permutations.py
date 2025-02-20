15e2fde9a76acda5c2a65cff229e3ddda452481f
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-20 10:58:57 -0500

    Time: 0 ms (100%), Space: 18.2 MB (14.55%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        s = set()
+        s = []
 
         def create(ind,arr,r):
 
             if len((arr))== len(nums):
-                s.add(tuple(arr[:]))
+                s.append(arr[:])
                 return
             for i in range(0,len(r)):
                 create(i,arr+[r[i]],r[:i]+r[i+1:])
 
 
         create(0,[],nums)
-        return list(s)
+        return (s)
         
