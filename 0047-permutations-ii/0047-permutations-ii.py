b00687506bc75df08ac5f403d60ecdd2818a235d
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-20 11:02:06 -0500

    Time: 7 ms (59.84%), Space: 18 MB (77.66%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,13 +1,16 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
         nums.sort()
-        res = set()
+        res = []
         def check(ind,arr,r):
             if len(arr)== len(nums):
-                res.add(tuple(arr[:]))
+                res.append(arr[:])
                 return
             
             for i in range(0,len(r)):
+                if i>0 and r[i]==r[i-1]:
+                    continue
                 check(i,arr+[r[i]],r[:i]+r[i+1:])
         check(0,[],nums)
-        return list(res)
+        return (res)
