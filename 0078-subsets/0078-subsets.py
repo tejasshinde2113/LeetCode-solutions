812dbd3b53b59d4132891af84cbad5be4b73ea0e
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-20 16:31:47 -0500

    Time: 4 ms (4.61%), Space: 17.9 MB (40.25%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,14 @@
-from typing import List
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        def backtrack(start, path):
-            res.append(path[:]) 
+
+        res =[]
+        def check(cnt,arr):
+            res.append(arr[:])
             
-            for i in range(start, len(nums)):
-                backtrack(i + 1, path + [nums[i]])  
+            for i in range(cnt,len(nums)):
+                check(i+1,arr+[nums[i]])
         
-        backtrack(0, [])
+        check(0,[])
         return res
+
+        
