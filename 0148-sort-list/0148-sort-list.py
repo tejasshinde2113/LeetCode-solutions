82ef5a1539845cb76c3fad9d79e5fbff066abb43
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-21 12:53:59 -0500

    Time: 183 ms (57.17%), Space: 32.8 MB (88.19%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -4,44 +4,47 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def findMid(self,head):
-        
-        slow = fast= head
-        prev = None
+
+    def findmid(self,head):
+
+        slow = head
+        fast = head.next
         while fast and fast.next:
-            prev = slow
             slow = slow.next
             fast = fast.next.next
         
-        temp = head
-        while True:
-            if temp == prev:
-                temp.next = None
-                break
-            temp = temp.next
-        return head,slow
-    
-    def compare(self, l1,l2):
-        cur = dummyHead = ListNode()
-        while l1 and l2:
-            if l1.val <= l2.val:
-                cur.next = l1
-                l1 = l1.next
+        sec = slow.next
+        slow.next = None
+
+        
+        return head, sec
+
+
+    def merge(self,left,right):
+        final = res = ListNode()
+        
+        while left and right:
+            if left.val <= right.val:
+                res.next = left
+                left= left.next
             else:
-                cur.next = l2
-                l2 = l2.next
-            cur = cur.next
-        cur.next = l1 or l2
-        return dummyHead.next
+                res.next = right
+                right = right.next
+            res = res.next
+        
+        res.next  = left or right
+        return final.next
+
 
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        first, second = self.findMid(head)
         
-        left = self.sortList(first)
-        right = self.sortList(second)
+        if not head or not head.next:
+            return head 
+        first,sec = self.findmid(head)
 
-        return self.compare(left,right)
+        left =self.sortList(first)
+        right = self.sortList(sec)
+
+        return self.merge(left,right)
 
         
