ba349db903b0a3d89e80c404b2c2c6d1b1f98b64
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-20 20:11:19 -0500

    Time: 2 ms (10.87%), Space: 18 MB (26.54%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+
+        res =[[0 for i in range(n)] for _ in range(n)]
+        print(res)
+        
+
+        left =0
+        right = n
+        top=0
+        bottom =n
+        cnt=1
+
+        while left <right and top<bottom:
+
+            for i in range(left,right):
+                res[top][i] = cnt
+                cnt+=1
+            top+=1
+
+            for i in range(top,bottom):
+                print(top,right-1)
+                # print(res)
+                res[i][right-1] = cnt
+                cnt+=1
+            right-=1
+
+            if not(left<right and top<bottom):
+                break
+
+            for i in range(right-1,left-1,-1):
+                res[bottom-1][i] = cnt
+                cnt+=1
+            bottom-=1
+
+            for i in range(bottom-1,top-1,-1):
+                res[i][left] = cnt
+                cnt+=1
+            left+=1
+        return res
