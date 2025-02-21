1aeba717c727439e5562a90ad8f293e0be99c451
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-21 09:06:46 -0500

    Time: 4 ms (51.53%), Space: 19 MB (86.23%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,37 +1,26 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 
-        adj =[[] for i in range(numCourses)]
+        adj = [[] for i in range(numCourses)]
 
-        prereq = [0 for a in range(numCourses)]
+        prereq = [0]*(numCourses)
 
         for course,pre in prerequisites:
-            adj[course].append(pre)
-            prereq[pre]+=1
-        
-        
-        
+            adj[pre].append(course)
+            prereq[course]+=1
         
 
-        zero = deque([])
-
-        for i,val in enumerate(prereq):
-            if not val:
-                zero.append(i)
+        zero=deque([i for i in range(len(prereq)) if prereq[i]==0])
 
         res =[]
 
         while zero:
-            temp = zero.popleft()
-            res.append(temp)
-
-            for val in adj[temp]:
-                prereq[val]-=1
-                if prereq[val]==0:
-                    zero.append(val)
+            course = zero.popleft()
+            res.append(course)
+            for pre in adj[course]:
+                prereq[pre]-=1
+                if prereq[pre]==0:
+                    zero.append(pre)
         
+        return  res if sum(prereq)==0 else []
         
-        return res[::-1] if sum(prereq) ==0 else []
-
-
-
