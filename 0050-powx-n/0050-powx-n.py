3066bf7b3003685749c914b48c68adb3d66c0300
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-22 14:18:08 -0500

    Time: 0 ms (100%), Space: 17.8 MB (50.42%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
+
+        neg = False
+        if n < 0:
+            neg= True
+            n=-n
+        total=1
+        while n>0:
+            if n%2 !=0:
+                total = total*x
+                n-=1
+            else:
+                x = x*x
+                n=n//2
+        return 1/total if neg else total
