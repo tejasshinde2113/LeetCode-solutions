8bb0125270c9900d4e99bf6a6d2069f2922dc9b8
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2025-02-22 13:43:38 -0500

    Time: 284 ms (82.84%), Space: 17.9 MB (57.84%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,36 +1,39 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        res=[]
-        fixed =[]
-        def recur(k,start,target):
 
-            if k != 2:
+
+        res =[]
+        arr=[]
+        def helper(start, k, target):
+            if k !=2:
                 for i in range(start,len(nums)-k+1):
-                    if i>start and nums[i] == nums[i-1]:
+                    if i>start and nums[i]==nums[i-1]:
                         continue
-                    fixed.append(nums[i])
-                    recur(k-1,i+1,target-nums[i])
-                    fixed.pop()
+                    
+                    arr.append(nums[i])
+                    helper(i+1,k-1,target - nums[i])
+                    arr.pop()
                 return
             
             p1=start
-            p2 = len(nums)-1
+            p2=len(nums)-1
             while p1<p2:
+                curr = nums[p1]+nums[p2]
 
-                currSum = sum([fixed[0],fixed[1],nums[p1],nums[p2]])
-                print(currSum)
-
-                if currSum == target:
-                    res.append([fixed[0],fixed[1],nums[p1],nums[p2]])
+                if curr < target:
                     p1+=1
-                    while p1<p2 and nums[p1]==nums[p1-1]:
-                        p1+=1
-                elif currSum < target:
-                    p1+=1
-                else:
+                elif curr > target:
                     p2-=1
+                else:
+                    res.append(arr+[nums[p1],nums[p2]])
+                    p1+=1
+                    while p1 < p2 and nums[p1]==nums[p1-1]:
+                        p1+=1
+                    p2-=1
+        
 
-        recur(4,0,target)
+
+        helper(0,4,target)
         return res
         
